70befe61313c066fb1923616df21965c6cacc159
Kevin Jones <kevinjones@redhat.com>
2016-05-07 10:25:13 -0400

    Modifying ospdirectorinstaller to add ansible installation and remove ahc-tools installation

--- ospdirectorinstall.sh
+++ ospdirectorinstall.sh
@@ -101,7 +101,12 @@ echo "Setting Repo Priorities"
 yum-config-manager --enable rhel-7-server-openstack-8-rpms --setopt="rhel-7-server-openstack-8-rpms.priority=1" && yum-config-manager --enable rhel-7-server-rpms --setopt="rhel-7-server-rpms.priority=1" && yum-config-manager --enable rhel-7-server-optional-rpms --setopt="rhel-7-server-optional-rpms.priority=1" && yum-config-manager --enable rhel-7-server-extras-rpms --setopt="rhel-7-server-extras-rpms.priority=1" && yum-config-manager --enable rhel-7-server-openstack-8-director-rpms --setopt="rhel-7-server-openstack-8-director-rpms.priority=1" && yum-config-manager --enable rhel-7-server-rh-common-rpms --setopt="rhel-7-server-rh-common-rpms.priority=1" && yum-config-manager --enable rhel-7-server-satellite-tools-6.1-rpms --setopt="rhel-7-server-satellite-tools-6.1-rpms.priority=1" && yum-config-manager --enable rhel-ha-for-rhel-7-server-rpms --setopt="rhel-ha-for-rhel-7-server-rpms.priority=1" && yum-config-manager --enable rhel-7-server-rhceph-1.3-osd-rpms --setopt="rhel-7-server-rhceph-1.3-osd-rpms.priority=1" && yum-config-manager --enable rhel-7-server-rhceph-1.3-mon-rpms --setopt="rhel-7-server-rhceph-1.3-mon-rpms.priority=1"
 
 echo "Updating system"
-yum install vim screen tree wget yum-plugin-priorities yum-utils facter ahc-tools openstack-utils git libguestfs-tools-c ansible -y && yum update -y
+yum install vim screen tree wget yum-plugin-priorities yum-utils facter openstack-utils git libguestfs-tools-c -y && yum update -y
+
+echo "Installing ansible"
+wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum install epel-release-latest-7.noarch.rpm -y
+yum install ansible -y
 
 mkdir -p /home/stack/{images,templates} 
 chown -R stack.stack /home/stack
