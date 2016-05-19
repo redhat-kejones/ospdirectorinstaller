48badad98049cd8a503b4f523a6a23c0f08ca5b6
Kevin Jones <kevinjones@redhat.com>
2016-05-18 23:58:56 -0400

    adding a few timeout changes for nova and ironic

--- 4_optimize.sh
+++ 4_optimize.sh
@@ -1,3 +1,6 @@
+sudo openstack-config --set /etc/nova/nova.conf DEFAULT rpc_response_timeout 600
+sudo openstack-config --set /etc/ironic/ironic.conf DEFAULT rpc_response_timeout 600
+
 sudo sed -i 's/#\(max_concurrent_builds\).*/\1=4/' /etc/nova/nova.conf
 sudo sed -i 's/#\(rpc_thread_pool_size\).*/\1=8/' /etc/ironic/ironic.conf
 for i in $(sudo systemctl | awk '/ironic|nova/{print$1}'); do sudo systemctl restart $i; done
