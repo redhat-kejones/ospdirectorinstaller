677ebf287ccf4aad563092e7f33424b86c968ede
Kevin Jones <kevinjones@redhat.com>
2016-10-13 16:45:52 -0400

    Fix crontab entry commands

--- 4_optimize.sh
+++ 4_optimize.sh
@@ -6,6 +6,6 @@ sudo sed -i 's/#\(rpc_thread_pool_size\).*/\1=8/' /etc/ironic/ironic.conf
 for i in $(sudo systemctl | awk '/ironic|nova/{print$1}'); do sudo systemctl restart $i; done
 
 echo "Adding cron jobs to flush keystone and heat databases (from tuning the undercloud)"
-crontab -l > /root/file; echo '0 04 * * * /bin/keystone-manage token_flush' >> /root/file; crontab /root/file
-crontab -l > /root/file; echo '0 04 * * * /bin/heat-manage purge_deleted -g days 1' >> /root/file; crontab /root/file
+sudo crontab -l > /tmp/file; sudo echo '0 04 * * * /bin/keystone-manage token_flush' >> /tmp/file; sudo crontab /tmp/file
+sudo crontab -l > /tmp/file; sudo echo '0 04 * * * /bin/heat-manage purge_deleted -g days 1' >> /tmp/file; sudo crontab /tmp/file
 
